3c410d7565f46b18250597c2d08c5b1a6474c0a9
GSolis <german_solis@intuit.com>
2016-07-12 16:03:55 -0500

    Cleaned up

--- app/reducers/timeline-reducer.js
+++ app/reducers/timeline-reducer.js
@@ -12,9 +12,9 @@ const timelineReducer = (state = initialState, action) => {
       break;
 
     case actionNames.ENTRY_ADDED:
-        // let last = state.timeline[0], align = 'left';
-        // if (last) align = last.align === 'left' ? 'right' : 'left';
-        // action.data.align = align;
+        let last = state.timeline[0], align = 'left';
+        if (last) align = last.align === 'left' ? 'right' : 'left';
+        action.data.align = align;
 
         return Object.assign({}, state, { timeline: [action.data, ...state.timeline] });
       break;
